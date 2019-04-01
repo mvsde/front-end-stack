831aa579895fe227cdcd3ab811a510bc96ae7dff
Fynn Becker <post@fynn.be>
2019-04-01 16:03:07 +0200

    chore: Output massaging

--- 04.vue/webpack.config.js
+++ 04.vue/webpack.config.js
@@ -1,13 +1,28 @@
 const path = require('path')
+const TerserPlugin = require('terser-webpack-plugin')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   devtool: false,
   entry: './hello.vue',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        cache: true,
+        parallel: true,
+        terserOptions: {
+          compress: false,
+          mangle: false,
+          keep_classnames: true,
+          keep_fnames: true
+        }
+      })
+    ]
   },
   module: {
     rules: [
--- 05.tree-shaking/webpack.config.js
+++ 05.tree-shaking/webpack.config.js
@@ -15,23 +15,11 @@ module.exports = {
         cache: true,
         parallel: true,
         terserOptions: {
-          compress: false,
           mangle: false,
           keep_classnames: true,
           keep_fnames: true
         }
       })
-    ],
-    splitChunks: {
-      cacheGroups: {
-        vendors: {
-          test: /[\\/]node_modules[\\/]/,
-          enforce: true,
-          name: 'vendors',
-          chunks: 'all'
-        }
-      }
-    },
-    runtimeChunk: 'single'
+    ]
   }
 }
