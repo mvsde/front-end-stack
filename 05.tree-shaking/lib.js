2535545591de803c277215f935dea584a497178f
Fynn Becker <post@fynn.be>
2019-04-01 15:21:35 +0200

    feat: Add tree shaking

---
+++ 05.tree-shaking/lib.js
@@ -0,0 +1,9 @@
+export function add (a, b) {
+  console.log('Add function')
+  return a + b
+}
+
+export function subtract (a, b) {
+  console.log('Substract function')
+  return a - b
+}
---
+++ 05.tree-shaking/main.js
@@ -0,0 +1,5 @@
+import { add } from './lib'
+import { foo } from './data'
+
+document.body.innerText = add(1, 2)
+console.log(foo)
